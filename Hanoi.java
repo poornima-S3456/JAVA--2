ee4b43c4b06afd490b528ebab20687137ce6d124
poornima-S3456 <prathasingh8536885@gmail.com>
2025-05-19 20:47:17 +0530

    msg

---
+++ Hanoi.java
@@ -0,0 +1,24 @@
+public class Hanoi {
+
+    public static void towerofhanoi( int n,String src,String helper,String dest  ){
+        //base case
+        if(n==1){
+
+           System.out.println( "transfer disk " + n + " from " + src + " to  " + dest);
+           return;
+        }
+        towerofhanoi( n-1,src, dest,helper);
+        System.out.println( "transfer disk  "+ n + " from  " + src + " to " + dest);
+          towerofhanoi( n-1,helper,src,dest);
+    
+}
+
+
+
+public static void main(String args[]){
+
+
+    int n=3;
+    towerofhanoi(n,"source","helper","Destination");
+}
+}
--- Pairing.java
+++ Pairing.java
@@ -15,7 +15,21 @@ public class Pairing {
          return (n-1)+totelway;
     }
 
+    public static void printBinaryString( int n,int lastplace,String str){
+        if(n==0){
+            System.out.println(str);
+            return ;
+        }
+
+        printBinaryString( n-1,0,str+"0");
+        if(lastplace==0){
+            printBinaryString(n-1,1,str+"1");
+
+       }
+    }
+
     public static void main(String[] args){
+        printBinaryString( 6, 0,"");
 
         System.out.println( " totelways is :"+Friendspairing(3));
     }
