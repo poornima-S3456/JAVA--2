8b0cbb3c722861dd3b4e209671f94d793360a113
poornima-S3456 <prathasingh8536885@gmail.com>
2025-05-26 00:18:42 +0530

    msg

--- TwoDArray.java
+++ TwoDArray.java
@@ -2,25 +2,56 @@ import java.util.*;
 
 
 public class TwoDArray {
-    public static void main( String args[]){
-        int matrix[][]= new int[3][3];
-        int n= matrix.length , m= matrix[0].length;
-        Scanner sc = new Scanner( System.in);
-        for( int i=0;i<n;i++){
 
-            for( int j=0;j<m;j++){
+public static void printDiagonals (int[][]arr){
 
-                matrix[i][j]=sc.nextInt();
-            }
-        } 
+int n = arr.length;
 
-        for( int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                System.out.print(matrix[i][j]+ " ");
-            }
-            System.out.println();
-        }
+System.out.println("primary diagonal: ");
+for(int i=0; i<n; i++){
 
-    }
-    
+    System.out.println(arr[i][i] + " ");
 }
+
+
+System.out.println( "\nSecondary diagonal: ");
+
+for(int i=0; i<n; i++){
+    System.out.println( arr[i][n-1-i]+ "  ");
+}
+
+}
+
+
+
+    public static void main( String args[]){
+        int arr[][]= {{1 , 2 , 3},
+                      { 4 , 5 , 6},
+                      { 7 , 8 , 9 }     };
+                      printDiagonals(arr);
+
+       }
+
+
+
+//         int matrix[][]= new int[3][3];
+//         int n= matrix.length , m= matrix[0].length;
+//         Scanner sc = new Scanner( System.in);
+//         for( int i=0;i<n;i++){
+
+//             for( int j=0;j<m;j++){
+
+//                 matrix[i][j]=sc.nextInt();
+//             }
+//         } 
+
+//         for( int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 System.out.print(matrix[i][j]+ " ");
+//             }
+//             System.out.println();
+//         }
+
+     }
+    
+ 
