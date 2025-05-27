fbe13ba907660957bfe71080d6ec9e2b14bea956
poornima-S3456 <prathasingh8536885@gmail.com>
2025-05-27 12:56:41 +0530

    msg

--- AL.java
+++ AL.java
@@ -1,6 +1,6 @@
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
+
  public class AL {
 
  public static void swap(ArrayList<Integer>list , int idx1, int idx2){
@@ -17,7 +17,6 @@ import java.util.Collections;
     ArrayList<Integer>list3= new ArrayList<>();
 
     for(int i=1;i<=5;i++){
-
       list1.add(i*1);//1,2,3,4,5
       list2.add(i*2);//2,4,6,8,10
       list3.add(i*3);//3,6,9,12,15
@@ -25,10 +24,9 @@ import java.util.Collections;
       mainList.add(list1);
       mainList.add(list2);
       mainList.add(list3);
-      System.out.println(mainList);
+      System.out.println(" mainlist is = "+ mainList);
 
       //nested loops
-
       for(int i=0;i<mainList.size();i++){
         ArrayList<Integer>currList= mainList.get(i);
 
@@ -41,12 +39,12 @@ import java.util.Collections;
      
 
 
-//    //ascending order(Sorting)
+    //ascending order(Sorting)
    Collections.sort(list2);
   System.out.println("ascending order : "+list2);
 
 
-// //descending order(Sorting)
+ //descending order(Sorting)
 Collections.sort(list2 , Collections.reverseOrder());
 System.out.println(" descending order : "+list2);
 
@@ -59,31 +57,29 @@ System.out.println(" descending order : "+list2);
 
 
   //  //find maximum in arraylist
-    // int max = Integer.MIN_VALUE;
-    // for(int i=0;i<list.size();i++){
-    //     if(max<list.get(i)){
-    //       max = list.get(i);
-    //     }
-    // }
-    // System.out.println(" max element "+max+ " " );
+    int max = Integer.MIN_VALUE;
+    for(int i=0;i<list1.size();i++){
+        if(max<list1.get(i)){
+          max = list1.get(i);
+        }
+    }
+    System.out.println(" max element = "+max+ " " );
+   System.out.println(" size of element = "+list1.size());
 
 
 
-    //System.out.println(list.size());
-
-    //print the number of array list
-
-    // for(int i=0;i<list.size();i++){
-    //     System.out.print(list.get(i)+" ");
-    // }
-    // System.out.println();
+  //  // print the number of array list
+  //   for(int i=0;i<list1.size();i++){
+  //       System.out.print(list1.get(i)+" ");
+  //   }
+  //   System.out.println();
 
 
-    //print reverse  O(n)time complexity
+   // print reverse  O(n)time complexity
 
-//     for(int i=list.size()-1;i>=0;i--){
-//         System.out.print(list.get(i)+" ");
+  //   for(int i=list2.size()-1;i>=0;i--){
+  //       System.out.print(list2.get(i)+" ");
     
-// }
-// System.out.println();
+//}
+//System.out.println();
 }}
--- Alist.java
+++ Alist.java
@@ -1,31 +1,103 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Alist {
 
     public static void main(String args[]){
 
-      ArrayList<Integer> list = new ArrayList<>();
 
-      //add operation
-      list.add(1);
-      list.add(2);
-      list.add(3);
-      list.add(4);
+      //using hashset
+      int[]arr ={ 1,2,2,42,4,5,3,53,45,3,5};
+  HashSet<Integer>set = new HashSet<>();
 
-      list.add( 1,9);
-     System.out.println(list);
+  int count =0;
+//   arr1.add(1);
+//   arr1.add(2);
+//   arr1.add(2);
+//   arr1.add(42);
+//  arr1.add(4);
+//  arr1.add(5);
+//  arr1.add(3);
+//  arr1.add(53);
 
-     //get operation
-    //  int element = list.get(2);
+//print all duplicate
+  for( int i=0;i<arr.length;i++){
+    if(set.contains(arr[i])){
+System.out.println(" duplicate element = " +arr[i]);
+count++;
+   }
+set.add( arr[i]);
+ }
+  System.out.println(set);
+  System.out.println( count);
+
+
+
+
+HashMap<Integer , Integer>map = new HashMap<>();
+map.put(1,1);
+map.put( 2,2);
+map.put(3,3);
+map.put(4,4);
+
+
+//Foreach loop
+map.forEach((key,value )->{
+System.out.println( key+"   "+value);
+
+});
+
+
+
+
+
+System.out.println( map.containsKey(1));
+System.out.println(map.get(2));
+//System.out.println(map.remove(3));
+System.out.println();
+
+for(int Key : map.keySet()){
+  if(Key>2){
+  System.out.println(Key);
+
+}}
+
+
+
+
+
+
+
+  // TreeSet<String>set1 = new TreeSet<>();
+  // set1.add("p");
+  // set1.add("q");
+  // set1.add("r");
+  // set1.add("P");
+  // set1.add("Q");
+  // System.out.print(set1);
+
+
+    //   ArrayList<Integer> list = new ArrayList<>();
+    //   //add operation
+    //   list.add(1);
+    //   list.add(2);
+    //   list.add(3);
+    //   list.add(4);
+
+    //   list.add( 1,9);
+    //   list.add(2,8);
+    //  System.out.println(list);
+
+    // // get operation
+    //  int element = list.get(3);
     //  System.out.println(element);
 
 
-    //  //remove element
+    // //  //remove element
     // list.remove(2);
-   //  System.out.println(list);
+    // System.out.println(list);
 
-     //set element
-    // list.set(2,10);
+    //  //set element
+    // // list.set(2,10);
     //  System.out.println(list);
 
 
