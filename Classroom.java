1bb001aae3292a4cba9a6c4c8b6448f0c0c22f57
poornima-S3456 <prathasingh8536885@gmail.com>
2025-05-29 22:19:47 +0530

    msg

---
+++ Classroom.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Classroom {
+    public static int Storewater(ArrayList<Integer>height){
+int maxwater = 0;
+
+for(int i=0;i<height.size();i++){
+
+    for(int j=i+1;j<height.size();j++){
+        int ht = Math.min(height.get(i) , height.get(j));
+        int width = j-i;
+        int currwater =  ht*width;
+        maxwater = Math.max(maxwater , currwater);
+    }
+}
+return maxwater;
+    }
+
+    public static void main(String args[]){
+
+        ArrayList<Integer>height = new ArrayList<>();
+        //1,8,6,2,5,4,8,3,7
+
+        height.add(1);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+
+        System.out.println(Storewater(height));
+        
+    }
+
+
+    
+}
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,16 +1,1 @@
-//     int arr[]={1,2,3,4,5,6};
-//     System.out.print("Print original array = ");
-//     printArray(arr);
-
-//  rotateByOne(arr);
-//   System.out.print("Array after rotation = ");
-//    printArray(arr);
- }
-
-
-
-
-
-
-    
-}
+height
