88df22ffab819c1673f591407ef080c835563ed6
poornima-S3456 <prathasingh8536885@gmail.com>
2025-06-01 22:38:12 +0530

    msg

--- DSA.java
+++ DSA.java
@@ -1,39 +1,83 @@
 
+import java.util.*;
+
 public class DSA {
 
-    public static void reverse(int arr[]){
+    public static void reverse(int arr[]) {
 
-        int start =0;
-        int end = arr.length-1;
+        int start = 0;
+        int end = arr.length - 1;
 
-        while(start<end){
+        while (start < end) {
 
-            //swap arr[start] and arr[end]
-             int temp= arr[start];
-             arr[start]=arr[end];
-             arr[end]= temp;
+            // swap arr[start] and arr[end]
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
 
-             start++;
-             end--;
+            start++;
+            end--;
         }
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
 
-int arr[]= {5,4,3,2,1};
-reverse(arr);
+        //input array size
 
-//print reverse array
+        System.out.print("enter the number of elements: ");
+        int n= sc.nextInt();
+        
+        int arr1[]= new int[n];
 
-System.out.println( "reverse an array in place :");
+        //input array element
 
-for(int i=0;i<arr.length;i++){
+        System.out.print("enter the element: ");
+        for(int i=0;i<n;i++){
+            arr1[i]= sc.nextInt();
 
-    System.out.print(arr[i] + " ");
-}
+        }
+        //input value of k
+        System.out.print("enter the value of k: ");
+        int k = sc.nextInt();
+
+        //sort the array
+        Arrays.sort(arr1);
+
+        //check if k is valid
+
+     if(k>0&& k<=n){
+
+        int kthMin = arr1[k-1];
+        int kthMax = arr1[n-k];
+
+        System.out.println("kth Min element: "+ kthMin);
+        System.out.println("kth Max element: "+kthMax);
+     }
+     else{
+
+        System.out.println(" Invalid value of k");
+     }
+     sc.close();
+
+
+
+
+
+
+
+
+
+
+        // int arr[] = { 5, 4, 3, 2, 1 };
+        // reverse(arr);
+
+        // // print reverse array
+        // System.out.println("reverse an array in place :");
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.print(arr[i] + " ");
+        //}
 
     }
 
-
-    
 }
