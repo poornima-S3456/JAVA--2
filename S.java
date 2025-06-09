3a44d04c835f73ebaccc94ff210ba16096937ce7
poornima-S3456 <prathasingh8536885@gmail.com>
2025-06-09 23:22:26 +0530

    msg

---
+++ S.java
@@ -0,0 +1,8 @@
+public class S  {
+    public static void main(String args[]){
+        String str = "hellow";
+        String reversed = new StringBuilder(str).reverse().toString();
+        System.out.println("reversed String : " + reversed);
+    }
+    
+}
