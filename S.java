20b5f23235d5e422798a6ce6cfe8e4bd2a0a54f1
poornima-S3456 <prathasingh8536885@gmail.com>
2025-06-10 23:11:07 +0530

    msg

--- S.java
+++ S.java
@@ -1,8 +1,34 @@
+import java.util.*;
+
 public class S  {
+
+    //check palindrome
     public static void main(String args[]){
-        String str = "hellow";
-        String reversed = new StringBuilder(str).reverse().toString();
-        System.out.println("reversed String : " + reversed);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the element:  ");
+        String input = sc.nextLine();
+
+        String Original = input.toLowerCase();
+        String reversed = "";
+
+        for(int i= Original.length()-1; i>=0;i--){
+            reversed += Original.charAt(i);
+
+        }
+        if(Original.equals(reversed)){
+            System.out.println("Palindrome....");
+        }
+        else{
+            System.out.println("not a Palindrome...");
+        }
+
+
+
+
+       //( reversed string)
+        // String str = "hellow";
+        // String reversed = new StringBuilder(str).reverse().toString();
+        // System.out.println("reversed String : " + reversed);
     }
     
 }
