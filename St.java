a361b3af5b260583806fb8e969110476465ee312
poornima-S3456 <prathasingh8536885@gmail.com>
2025-06-19 12:06:18 +0530

    msg

--- St.java
+++ St.java
@@ -141,9 +141,40 @@ public class St {
         return combined.contains(s2);
     }
 
+
+
+    //subsequence
+      public static boolean isSubsequence(String s1, String s2) {
+        int i = 0, j = 0;
+        while (i < s1.length() && j < s2.length()) {
+            if (s1.charAt(i) == s2.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == s1.length();
+    }
+
+    
        
+
         
     public static void main(String[] args) {
+         String s1 = "abc";
+        String s2 = "ahbgdc";
+
+        if (isSubsequence(s1, s2)) {
+            System.out.println(s1 + " is a subsequence of " + s2);
+        } else {
+            System.out.println(s1 + " is NOT a subsequence of " + s2);
+        }
+   
+
+
+
+
+
+
 
  String str1 = "waterbottle";
         String str2 = "erbottlewat";
