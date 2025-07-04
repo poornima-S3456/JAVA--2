fd21aefb5138b01787666e50ac4ce9d27c27d3ec
poornima-S3456 <prathasingh8536885@gmail.com>
2025-07-04 23:16:25 +0530

    msg

--- DSA.java
+++ DSA.java
@@ -102,9 +102,39 @@ public class DSA {
         }
     }
 
+ public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap adjacent elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // If no swaps happened, array is sorted
+            if (!swapped) break;
+        }
+    }
+
     public static void main(String args[]) {
-        int arr6[] = { 16, 17, 4, 3, 5, 2 };
-        findLeader(arr6);
+int[] arr = {64, 25, 12, 22, 11};
+        bubbleSort(arr);
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }  
+        
+        
+        // int arr6[] = { 16, 17, 4, 3, 5, 2 };
+        // findLeader(arr6);
 
         // int arr[]= { 1,2,3,4,5,6,7,9,10};
         // int n=10;
