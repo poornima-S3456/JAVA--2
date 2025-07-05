6e244cab1129bafc927091128fa68c4c6b5cfca6
poornima-S3456 <prathasingh8536885@gmail.com>
2025-07-05 23:04:26 +0530

    msg

--- DSA.java
+++ DSA.java
@@ -124,13 +124,91 @@ public class DSA {
         }
     }
 
+    public static void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+
+        // Check if first row has zero
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        // Check if first column has zero
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+
+        // Use first row and column as markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Set zeroes based on markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Update first row
+        if (firstRowZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Update first column
+        if (firstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+    // Example usage
+   
+       
+   
+
     public static void main(String args[]) {
-int[] arr = {64, 25, 12, 22, 11};
-        bubbleSort(arr);
-        System.out.println("Sorted array:");
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }  
+// int[] arr = {64, 25, 12, 22, 11};
+//         bubbleSort(arr);
+//         System.out.println("Sorted array:");
+//         for (int num : arr) {
+//             System.out.print(num + " ");
+//         }  
+
+         int[][] matrix = {
+            {1, 2, 3},
+            {4, 0, 6},
+            {7, 8, 9}
+        };
+
+        setZeroes(matrix);
+
+        // Print result
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
         
         
         // int arr6[] = { 16, 17, 4, 3, 5, 2 };
