175696780cc2a9db9e1f5c65cdeeb6766bb490b6
poornima-S3456 <prathasingh8536885@gmail.com>
2025-07-08 22:17:56 +0530

    msg

--- Basic.java
+++ Basic.java
@@ -1,5 +1,10 @@
+import java .util.*;
+
 public class Basic {
+        
     public static void main(String[] args) {
+
+        //code 1
         int[] numbers = {45, 32, 89, 12, 67, 90, 23};
 
         int max = numbers[0]; // Assume first element is the max
@@ -11,6 +16,29 @@ public class Basic {
         }
 
         System.out.println("Maximum element in the array: " + max);
+
+
+        //code2
+         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+            System.out.println(num + " is a Prime number.");
+        else
+            System.out.println(num + " is NOT a Prime number.");
     }
 }
 
