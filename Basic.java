63468089abd55eec306c2ea9b1c12df2e1240f60
poornima-S3456 <prathasingh8536885@gmail.com>
2025-07-12 23:41:44 +0530

    msg

--- Basic.java
+++ Basic.java
@@ -90,6 +90,29 @@ String input = "Java is a powerful language";
         System.out.println("Maximum element in the array: " + max);
 
 
+
+        
+        
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        boolean isPrime = true;
+
+        if (n <= 1) isPrime = false;
+        else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+            System.out.println(n + " is a Prime number.");
+        else
+            System.out.println(n + " is not a Prime number.");
+
+
         //code2
     //      Scanner sc = new Scanner(System.in);
     //     System.out.print("Enter a number: ");
